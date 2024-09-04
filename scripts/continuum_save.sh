75d1ec39866e45a04929c07859160c93c865129d
jaclu <jacob.lundqvist@gmail.com>
2024-09-04 12:11:11 +0200

    using a timestamp file to reduce load

--- scripts/continuum_save.sh
+++ scripts/continuum_save.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source "$CURRENT_DIR/helpers.sh"
 source "$CURRENT_DIR/variables.sh"
@@ -39,7 +39,7 @@ acquire_lock() {
 	# save to be running, otherwise we can get corrupted saved state.
 	local lockdir_prefix="/tmp/tmux-continuum-$(current_tmux_server_pid)-lock-"
 	# The following implements a lock that auto-expires after 100...200s.
-	local lock_generation=$((`date +%s` / 100))
+	local lock_generation=$(($(date +%s) / 100))
 	local lockdir1="${lockdir_prefix}${lock_generation}"
 	local lockdir2="${lockdir_prefix}$(($lock_generation + 1))"
 	if mkdir "$lockdir1"; then
@@ -49,10 +49,28 @@ acquire_lock() {
 			return 0
 		fi
 	fi
-	return 1  # Someone else has the lock.
+	return 1 # Someone else has the lock.
 }
 
 main() {
+	# This script may be triggered frequently, as often as every 10 seconds or less.
+	# On slower systems, this frequent execution can cause noticeable delays, as
+	# the script performs substantial processing that might take up to a second.
+	# To improve responsiveness and reduce system load, a timestamp file is used
+	# to limit full processing to once per minute.
+	local f_next_check_timestamp="$CURRENT_DIR/continuum_next_check.timestamp"
+	local t_now
+	t_now="$(current_timestamp)"
+	[ -f "$f_next_check_timestamp" ] && {
+		local next_check
+		read -r next_check <"$f_next_check_timestamp" # Using read instead of cat for efficiency
+		# Check if the timestamp file contains a valid timestamp (positive integer)
+		if [[ -n "$next_check" && "$next_check" =~ ^[0-9]+$ ]]; then
+			[ "$t_now" -lt "$next_check" ] && return
+		fi
+	}
+	echo "$((t_now + 60))" >"$f_next_check_timestamp"
+
 	if supported_tmux_version_ok && auto_save_not_disabled && enough_time_since_last_run_passed && acquire_lock; then
 		fetch_and_run_tmux_resurrect_save_script
 	fi
